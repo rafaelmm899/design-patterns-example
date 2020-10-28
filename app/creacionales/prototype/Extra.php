f9ab83ebded14add32552a75973b0794370961f5
Jose Mata <rafaelmm899@hotmail.com>
2020-10-27 21:22:05 -0300

    prototype pattern

---
+++ app/creacionales/prototype/Extra.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\creacionales\prototype;
+
+
+use MyCLabs\Enum\Enum;
+
+class Extra extends Enum
+{
+    public const CEBOLLA = 'cebolla';
+    public const QUESO = 'queso';
+    public const MAYONESA = 'mayonesa';
+    public const PEPINO = 'pepino';
+    public const HUEVO = 'huevo';
+}
---
+++ app/creacionales/prototype/Size.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace App\creacionales\prototype;
+
+
+use MyCLabs\Enum\Enum;
+
+class Size extends Enum
+{
+    public const MEDIO = 'medio';
+    public const GRANDE = 'grande';
+    public const EXTRA_GRANDE = 'extraGrande';
+}
